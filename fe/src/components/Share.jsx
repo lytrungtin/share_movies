ea8c7ffc60d8e7ee0e23d6eeaff0bd4757a30cb6
Tin Ly <trungtin.sotech@gmail.com>
2023-03-30 11:35:45 +0700

    update styling shared view

--- fe/src/components/Share.jsx
+++ fe/src/components/Share.jsx
@@ -1,4 +1,6 @@
 import React, { useState, useEffect } from 'react'
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
+import { icon } from '@fortawesome/fontawesome-svg-core/import.macro'
 const Share = () => {
   const [items, setItems] = useState([])
 
@@ -39,19 +41,36 @@ const Share = () => {
            </div>
          </div>
          <div className="container">
-            <div className="col-12 text-end">
-            {items.map((elem, index) => {
+            <div className="col-12 text-start">
+            {items.map((share, index) => {
               return (
                     <div
                       className="row border rounded shadow p-3 mb-3 bg-white rounded  p-2"
-                      key={elem.id}
+                      key={share.id}
                     >
                       <div className="col-12 d-flex justify-content-between align-items-center">
-                        <div>
-                          <h4>{elem.title}</h4>
-                          <p>{elem.description}</p>
+                        <div className="col-md-6 videoWrapper">
+                          <iframe
+                            src={`https://www.youtube.com/embed/${share.uid}?rel=0`}
+                            frameBorder="0"
+                            allowFullScreen
+                          />
                         </div>
+                        <div className="col-md-6">
+                          <p>
+                            <span className="title">{share.title}</span>
+                          </p>
+                          <p>
+                            <span className="user">Shared by: {share.user.email || 0}</span>
+                          </p>
+                          <p>
+                            {share.likes} <FontAwesomeIcon icon={icon({name: 'thumbs-up', style: 'regular'})} />
+                            {share.dislikes} <FontAwesomeIcon icon={icon({name: 'thumbs-down', style: 'regular'})} />
+                          </p>
+                          <span>Description:</span>
+                          <p className="description">{share.description.slice(0, 150)}</p>
                         </div>
+                      </div>
                     </div>
               )
             })}
