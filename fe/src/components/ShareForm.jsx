e05295d82b5ff8861c3c83672c7896b83e164f62
Tin Ly <trungtin.sotech@gmail.com>
2023-03-30 17:45:17 +0700

    add hanlde share submit to shareform

--- fe/src/components/ShareForm.jsx
+++ fe/src/components/ShareForm.jsx
@@ -4,13 +4,30 @@ const ShareForm = () => {
   const handleInputUrl = (e) => {
     setinputUrl(e.target.value)
   }
+
+  const handleShareSubmit = async (event) => {
+    event.preventDefault();
+    const token = localStorage.getItem('token');
+    await fetch('http://localhost:3000/shares', {
+      method: 'POST',
+      headers: { 'Content-Type': 'application/json', 'Authorization': `Bearer ${token}` },
+      body: JSON.stringify({ share: { url: inputUrl } }),
+    })
+    .then(async (response) => {
+        const data = await response.json();
+      })
+      .catch((error) => {
+        console.error('Error:', error);
+      });
+  };
+
   return (
     <div className="container border rounded d-flex justify-content-center shadow p-3 mb-5 bg-white rounded">
     <div className="row">
       <div className="text-center">
         <h2>Share a Youtube movie</h2>
       </div>
-      <form className="col-12 p-2" >
+      <form className="col-12 p-2" onSubmit={handleShareSubmit}>
         <label htmlFor="title" className="my-2">
           Youtube URL
         </label>
