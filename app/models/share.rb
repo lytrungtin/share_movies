174f085ef4b5b331826ee021db6f713e121b32aa
Tin Ly <trungtin.sotech@gmail.com>
2023-03-30 18:54:48 +0700

    update Share validation process

--- app/models/share.rb
+++ app/models/share.rb
@@ -5,9 +5,12 @@ class Share < ApplicationRecord
 
   belongs_to :user
   default_scope -> { order(created_at: :desc) }
-  validates :url, presence: true, format: YT_LINK_FORMAT, uniqueness: { case_sensitive: false }
+  validates :url, presence: true, format: YT_LINK_FORMAT,
+                  uniqueness: { case_sensitive: false, message: 'This URL is already taken' }
+  validates :uid, uniqueness: { case_sensitive: false, message: 'This video is already taken' }
+  validates :published_at, presence: { message: 'This video is unavailable' }
 
-  before_save :extract_video_info
+  before_validation :extract_video_info
 
   private
 
@@ -19,7 +22,7 @@ class Share < ApplicationRecord
     self.likes = video.like_count
     self.dislikes = video.dislike_count
     self.published_at = video.published_at
-  rescue Yt::Errors::NoItems
-    self.title = ''
+  rescue StandardError
+    errors.add(:url, 'This video is invalid')
   end
 end
