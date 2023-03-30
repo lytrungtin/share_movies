4c80471fa5bdeaca1e2cefd959a0698359ee05b6
Tin Ly <trungtin.sotech@gmail.com>
2023-03-30 11:33:55 +0700

    update shared movies listing to include user email

--- app/controllers/shares_controller.rb
+++ app/controllers/shares_controller.rb
@@ -5,10 +5,10 @@ class SharesController < ApplicationController
   def index
     page = params[:page] || 1
     per_page = params[:per_page] || 10
-    shares = Share.order(created_at: :desc).page(page).per(per_page)
+    shares = Share.includes(:user).order(created_at: :desc).page(page).per(per_page)
     total_pages = shares.total_pages
 
-    render json: { data: shares, meta: { total_pages: } }
+    render json: { data: shares.as_json(include: { user: { only: [:email] } }), meta: { total_pages: } }
   end
 
   def create
