3cf932f84131035377ecce45d76778ccba5cc57a
Tin Ly <trungtin.sotech@gmail.com>
2023-03-31 12:39:58 +0700

    config simplecov to check coverage

--- test/test_helper.rb
+++ test/test_helper.rb
@@ -4,6 +4,22 @@ ENV['RAILS_ENV'] ||= 'test'
 require_relative '../config/environment'
 require 'rails/test_help'
 
+
+ENV['RAILS_ENV'] ||= 'test'
+require 'simplecov'
+require 'simplecov_json_formatter'
+
+# Generate HTML and JSON reports
+SimpleCov.formatters = SimpleCov::Formatter::MultiFormatter.new([
+                                                                  SimpleCov::Formatter::HTMLFormatter,
+                                                                  SimpleCov::Formatter::JSONFormatter
+                                                                ])
+SimpleCov.start do
+  enable_coverage :branch
+end
+require_relative '../config/environment'
+require 'rails/test_help'
+
 module ActiveSupport
   class TestCase
     # Run tests in parallel with specified workers
